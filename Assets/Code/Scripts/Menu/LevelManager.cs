06de38f1292ff01af3ee49524e7f661cd1daeaa8
geropython <72422102+geropython@users.noreply.github.com>
2023-08-31 20:40:44 -0300

    menu host

--- Assets/Code/Scripts/Menu/LevelManager.cs
+++ Assets/Code/Scripts/Menu/LevelManager.cs
@@ -3,14 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameObject _panelNet;
     public GameObject _panelSplash;
     public void QuitGame()
     {
         Application.Quit();
     }
+    private void Start()
+    {
+        _panelNet.SetActive(false);
+    }
+    private void Update()
+    {
+        if (_panelNet== true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            _panelNet.SetActive(false);
+        }
+    }
+    
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        _panelNet.SetActive(true);
     }
     public void Menu()
     {
@@ -20,9 +33,4 @@ public class LevelManager : MonoBehaviour
     {
         _panelSplash.SetActive(false);
     }
-    public void Options()
-    {
-       
-    }
 }
-
