ebdc12f8fbb0ca5ab66de6748088b4e0c3541964
geropython <72422102+geropython@users.noreply.github.com>
2023-09-26 15:01:11 -0300

    animacion crash

--- Assets/Code/Scripts/PlayerScripts/PlayerAnim.cs
+++ Assets/Code/Scripts/PlayerScripts/PlayerAnim.cs
@@ -6,33 +6,23 @@ using UnityEngine.UIElements;
 
 public class PlayerAnim : NetworkBehaviour
 {
-    //public Animator anim;
-    //PlayerModel _playerModel;
-    //Rigidbody _rb;
-    //private void Awake()
-    //{
-    //    _rb = GetComponent<Rigidbody>();
-    //    _playerModel = GetComponent<PlayerModel>();
-    //}
-    //private void Start()
-    //{
-    //    if (!IsOwner)
-    //    {
-    //        enabled = false;
-    //    }
-    //    else
-    //    {
-    //        _playerModel.score.OnValueChanged += OnScoreChange;
-    //    }
-    //}
-    //void Update()
-    //{
-    //    if (!IsOwner) return;
-    //    anim.SetFloat("Vel", _rb.velocity.magnitude);
-    //}
-    //void OnScoreChange(int p, int n)
-    //{
-    //    if (!IsOwner) return;
-    //    anim.SetTrigger("Spin");
-    //}
+    public Animator anim;
+
+    private void Awake()
+    {
+    }
+    private void Start()
+    {
+        if (!IsOwner)
+        {
+            enabled = false;
+        }
+        else
+        {
+        }
+    }
+    void Update()
+    {
+        if (!IsOwner) return;
+    }
 }
